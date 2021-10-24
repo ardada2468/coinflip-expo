51fde93a574372a4e02ef43f82ec6fca5de7033f
ardada2468 <ardada2468@gmail.com>
2021-10-23 23:16:59 -0400

    init

--- App.tsx
+++ App.tsx
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native'
+import { Button, Title } from 'react-native-paper'
+interface Icoin {
+  heads?: number;
+  tails?: number;
+  coin?: String;
+}
+class App extends Component <Icoin>{
+  state = {
+    coin: "please filp the coin",
+    heads: 0,
+    tails: 0
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  onPress = () => {
+    let rand: any = Math.random()*10
+    let value: String = "tails"
+    if(rand > 4.5){
+      value = "heads"
+      this.setState({
+        heads: this.state.heads+1,
+        coin: value
+      })
+    }else{
+    this.setState({
+      tails: this.state.tails+1,
+      coin: value
+    })
+  }
+    // console.log({rand,value})
+  }
+
+
+
+  getResultStyler(){
+    if(this.state.coin == "tails"){
+      return styles.result1;
+    }else return styles.result2;
+  }
+
+ render() {
+    return (
+      <View style={[styles.container, this.getResultStyler()]}>
+        <Text style={styles.info}># Heads: {" " + this.state.heads}</Text>
+        <Text style={styles.info}># Tails:  { " " + this.state.tails}</Text>
+        <View>
+          {/* <Text>
+            It is { this.state.coin } !
+          </Text> */}
+          <Title style={[this.getResultStyler(), styles.commonResult]}>It is { this.state.coin } !</Title>
+          <Button onPress={this.onPress} style={styles.button}>
+            <Text>Flip Coin</Text>
+          </Button>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  button: {
+    // alignItems: 'center',
+    // backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginTop:10,
+  },
+  info: {
+    padding: "10px",
+    margin: "5px",
+  },
+  result1: {
+    backgroundColor: "#B7D1F8",
+
+  },
+  result2: {
+    backgroundColor: "#DFD2F4",
+  },
+  commonResult:{
+    padding: "10px",
+    margin:"10px",
+    borderRadius: 10
+  }
+
+})
+
+export default App;
